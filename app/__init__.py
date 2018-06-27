a14425ca70873ce02b45e21a7a1fb90ded8fe373
Phillip <phillip.preechakul.com>
2018-06-27 15:21:55 -0700

    update secret keys to the env

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+import os
 
 socketio = SocketIO()
 
@@ -8,7 +9,7 @@ def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
     app.debug = debug
-    app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    app.config['SECRET_KEY'] = os.environ.get('CHATAPP_SECRET_KEYS')
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
