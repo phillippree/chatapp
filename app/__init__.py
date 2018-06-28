9a1167628a5df4a2541ca392d13597e3acb36e86
Phillip <phillip.preechakul.com>
2018-06-28 14:03:41 -0700

    added flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
 import os
 
 socketio = SocketIO()
@@ -9,6 +10,7 @@ def create_app(debug=False):
     """Create an application."""
     app = Flask(__name__)
     app.debug = debug
+    Bootstrap(app)
     app.config['SECRET_KEY'] = os.environ.get('CHATAPP_SECRET_KEYS')
 
     from .main import main as main_blueprint
